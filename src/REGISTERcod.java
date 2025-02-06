40a637d211fbac93adb3f86894b499cd9218b819
ArghaGhosh1 <arghaghosh276@hotmail.com>
2025-02-06 12:11:12 +0530

    init commit

--- src/REGISTERcod.java
+++ src/REGISTERcod.java
@@ -3,7 +3,7 @@
 import java.sql.*;
 import java.util.Scanner;
 
-public class REGISTERcode
+public class REGISTERcod
 {
     public static void main(String[] args) throws ClassNotFoundException
     {
